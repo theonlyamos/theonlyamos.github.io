440afdc67b6e23c05ed457b44f21fa76cf4ca393
jamesstefens18 <fireisaverywickedmaster@gmail.com>
2018-12-20 18:45:51 +0000

    added colors to jots

--- pwa/jotter/app.js
+++ pwa/jotter/app.js
@@ -14,6 +14,16 @@
     let app = {
         save: saveJot,
         load: loadJots,
+        colors: {
+            "primary": {"bg": "bg-primary", "color": "text-primary"},
+            "secondary": {"bg": "bg-secondary", "color": "text-dark"},
+            "info": {"bg": "bg-info", "color": "text-info"},
+            "warning": {"bg": "bg-warning", "color": "text-info"},
+            "danger": {"bg": "bg-danger", "color": "text-warning"},
+            "dark": {"bg": "bg-dark", "color": "text-light"},
+            "light": {"bg": "bg-light", "color": "text-dark"},
+            "success": {"bg": "bg-success", "color": "text-light"},
+        }
     }
 
     if('serviceWorker' in navigator) {
@@ -22,6 +32,9 @@
 
     document.addEventListener('click', function(e){
         if (e.target.matches('.add-button')) {
+            if (!document.getElementById("deleteButton").classList.contains("invisible")){
+                document.getElementById("deleteButton").classList.toggle('invisible')
+            }
             var editor = document.getElementById('editor');
             editor.style.display = 'flex'
             return;
@@ -42,47 +55,104 @@
             editor.style.display = 'none'
             return;
         }
+        else if (e.target.matches('.jot-body')) {
+            var parent = getClosest(e.target, '.card')
+            for (let i = 0; i<parent.classList.length; i++){
+                if (parent.classList[i].indexOf('bg-') > -1){
+                    var c = parent.classList[i]
+                }
+            }
+            document.getElementById("content").value = e.target.textContent;
+            var time = parent.querySelector('.jot-time').textContent;
+            if (document.getElementById("deleteButton").classList.contains("invisible")){
+                document.getElementById("deleteButton").classList.toggle('invisible')
+            }
+            var editor = document.getElementById('editor');
+            editor.setAttribute("data-index", parent.getAttribute("data-index"))
+            editor.style.display = 'flex'
+        }
+        else if (e.target.matches("#color-tool")) {
+            var colors = e.target.getAttribute("data-colors")
+            var editor = document.getElementById('editor');
+            editor.setAttribute("data-colors", colors)
+        }
     });
 
 
 
 
+
     /* app functions */
+    function getClosest (elem, selector) {
+        for ( ; elem && elem !== document; elem = elem.parentNode ) {
+            if ( elem.matches( selector ) ) return elem;
+        }
+        return null;
+    };
 
     function saveJot(){
+        var editor = document.getElementById('editor');
         var content = document.getElementById("content").value;
         document.getElementById("content").value = '';
-        var time = new Date().toDateString();
-        var template = document.getElementById('cardTemplate');
-
+        var time = new Date().toGMTString();
+        var colors = editor.getAttribute("data-colors")
         let jots = localStorage.getItem('jots')
         if (!jots) {
             jots = []
         }else{
             jots = JSON.parse(jots)
         }
-        jots.push({"time": time, "content": content})
         
-        localStorage.setItem('jots', JSON.stringify(jots))
+        if (editor.hasAttribute("data-index")){
+            
+            var index = parseInt(editor.getAttribute("data-index"))
+            console.log(jots, index)
+            var jot = jots[index]
+            jot.time = time
+            jot.content = content
+            jot.colors = colors
+
+            jots[index] = jot
+            localStorage.setItem("jots", JSON.stringify(jots))
+            jots = localStorage.getItem("jots")
+
+            app.load(jots)
+        }
+        else {
+            var template = document.getElementById('cardTemplate');
+
+            
+            jots.push({"time": time, "content": content, "colors": colors})
+            
+            localStorage.setItem('jots', JSON.stringify(jots))
 
 
-        var card = template.cloneNode([true]);
-        card.removeAttribute('hidden')
-        card.removeAttribute('id')
-        card.querySelector(".jot-time").textContent = time;
-        card.querySelector(".jot-body").textContent = content;
-        
-        document.getElementById("jotsContainer").prepend(card)
+            var card = template.cloneNode([true]);
+            card.removeAttribute('hidden')
+            card.removeAttribute('id')
+            card.classList.add(bg)
+            card.querySelector(".jot-time").textContent = time;
+            card.querySelector(".jot-body").textContent = content;
+            
+            document.getElementById("jotsContainer").prepend(card)
+        }
     }
 
     function loadJots(jots){
         jots = JSON.parse(jots)
+        document.getElementById("jotsContainer").innerHTML = ""
         for (let i = 0; i < jots.length; i++) {
             var jot = jots[i]
             var template = document.getElementById('cardTemplate');
             var card = template.cloneNode([true]);
             card.removeAttribute('hidden')
             card.removeAttribute('id')
+            card.setAttribute("data-index", i)
+            if (jot.hasOwnProperty("colors")) {
+                card.classList.add(app.colors[jot.colors].bg)
+                card.querySelector(".jot-time").classList.add(app.colors[jot.colors].color)
+                card.querySelector(".jot-body").classList.add(app.colors[jot.colors].color)
+            }
             card.querySelector(".jot-time").textContent = jot.time;
             card.querySelector(".jot-body").textContent = jot.content;
 
