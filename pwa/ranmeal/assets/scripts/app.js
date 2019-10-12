9feda8145fe7e9dbd423ff5a73083c19bb5af0bc
jamesstefens18 <fireisaverywickedmaster@gmail.com>
2019-10-12 15:52:33 +0000

    added ingredients and measurements

--- pwa/ranmeal/assets/scripts/app.js
+++ pwa/ranmeal/assets/scripts/app.js
@@ -1,5 +1,3 @@
-
-
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('./service-worker.js')
@@ -17,7 +15,9 @@ const getMeal = () => {
       let bg = document.getElementById('background');
       let thumbnail = document.getElementById('card-img');
       let title = document.getElementById('card-title');
+      let ingredients = document.getElementById('meal-ingredients');
       let instructs = document.getElementById('meal-instructions');
+      let measurements = document.getElementById('meal-measurements');
 
       title.innerHTML = meal['strMeal'] + '<hr>';
 
@@ -27,7 +27,18 @@ const getMeal = () => {
       bg.style.backgroundSize = "cover";
 
       thumbnail.src = meal['strMealThumb'];
-      instructs.innerText = meal['strInstructions'];
+      instructs.innerHTML = meal['strInstructions'];
+
+      let ingreds = '';
+      let measures = '';
+
+      for (var i = 1; i < 19; i++) {
+        ingreds += meal[`strIngredient${i}`] + ",";
+        measures += meal[`strMeasure${i}`] + ",";
+      }
+
+      ingredients.innerHTML = ingreds;
+      measurements.innerText = measures;
 
     })
     .catch(e => {
@@ -39,4 +50,17 @@ getMeal();
 
 document.getElementById("fab").addEventListener("click", () => {
   getMeal();
+})
+
+document.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === 'strong') {
+    if (e.target.parentNode.parentNode.nextElementSibling.classList.contains("hide")) {
+      e.target.parentNode.parentNode.nextElementSibling.classList.remove("hide");
+      e.target.parentNode.parentNode.nextElementSibling.classList.add("show");
+    } 
+     else {
+      e.target.parentNode.parentNode.nextElementSibling.classList.remove("show");
+      e.target.parentNode.parentNode.nextElementSibling.classList.add("hide");
+    }
+  }
 })
